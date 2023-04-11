4f9ad20ec7fca2a617b0935051f58472b1748124
Vorn-Hagen <thibaut.francois@edu.devinci.fr>
2023-04-11 13:36:15 +0200

    :sparkles: Add all characters texts

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ Draggable.create(r2d2,{
     type:'x,y',
     onDrag : function(){
         if(this.hitTest(point, '10'))
-        console.log('r2d2'), title.innerHTML ='r2d2', msg.innerHTML ='lorem'
+        console.log('r2d2'), title.innerHTML ='R2-D2', msg.innerHTML ='A resourceful and loyal astromech droid, R2-D2 served Padmé Amidala, Anakin Skywalker and Luke Skywalker in turn, showing great bravery in rescuing his masters and their friends from many perils. '
     },
 
     onDragEnd : function(){
@@ -50,7 +50,7 @@ Draggable.create(luc,{
     type:'x,y',
     onDrag : function(){
         if(this.hitTest(point, '10'))
-        console.log('luc'), title.innerHTML ='luc', msg.innerHTML ='lorem'
+        console.log('luke'), title.innerHTML =' Luke Skywalker', msg.innerHTML ='Luke Skywalker is the main protagonist of the Star Wars original trilogy. The son of famous Jedi Knight Anakin Skywalker, he was left on Tatooine with his aunt and uncle after the fall of the Jedi Order. 19 years later, he encountered former Jedi Master Obi-Wan Kenobi (Under the alias "Ben Kenobi") and joined the Rebel Alliance to bring down the Galactic Empire.'
     },
 
     onDragEnd : function(){
@@ -64,7 +64,7 @@ Draggable.create(vador,{
     type:'x,y',
     onDrag : function(){
         if(this.hitTest(point, '10'))
-        console.log('vador'), title.innerHTML ='vador', msg.innerHTML ='lorem'
+        console.log('vador'), title.innerHTML ='Darth Vader', msg.innerHTML ='Darth Vader is a Dark Lord of the Sith and the apprentice of Emperor Palpatine, ruler of the Galactic Empire. Vader was once the esteemed Jedi Knight Anakin Skywalker, whose unparalleled midichlorian count labeled him as the Chosen One destined to bring balance to the Force. However, the numerous tragedies he suffered during his service to the Jedi Order in the Clone Wars took their toll on Skywalker, making the young knight desperate to find a way to protect his lover Padme Amidala. This opened the Chosen One to the manipulations of Darth Sidious, who turned the newly christened Vader against the Jedi Order, slaughtering numerous Jedi and their younglings as part of Order 66.'
     },
 
     onDragEnd : function(){
@@ -78,7 +78,7 @@ Draggable.create(trooper,{
     type:'x,y',
     onDrag : function(){
         if(this.hitTest(point, '10'))
-        console.log('trooper'), title.innerHTML ='trooper', msg.innerHTML ='lorem'
+        console.log('trooper'), title.innerHTML ='Stormtrooper  ', msg.innerHTML ='A Stormtrooper was a member of the elite shock troops in the Imperial Army, placed under the Stormtrooper Corps, the land-based forces of the Galactic Empire\'s Imperial Army. They were particularly distinguished by their loyalty to the Emperor Sheev Palpatine, the founder of the Empire, and were reputedly incapable of betraying either him or the Imperial regime.'
     },
 
     onDragEnd : function(){
@@ -92,7 +92,7 @@ Draggable.create(solo,{
     type:'x,y',
     onDrag : function(){
         if(this.hitTest(point, '10'))
-        console.log('solo'), title.innerHTML ='solo', msg.innerHTML ='lorem'
+        console.log('solo'), title.innerHTML ='Han Solo', msg.innerHTML ='Han Solo was one of the many smugglers that worked during the Galactic Empire\'s reign over the galaxy.<br>He and his pal, Chewbacca, ended up getting involved with the Rebellion when they were hired by Luke and Obi-Wan to transport them to Alderaan. But it turned out that the planet was destroyed by The Death Star, and they ended up boarding the space station.'
     },
 
     onDragEnd : function(){
@@ -107,7 +107,7 @@ Draggable.create(baka,{
     type:'x,y',
     onDrag : function(){
         if(this.hitTest(point, '10'))
-        console.log('baka'), title.innerHTML ='chewbacca', msg.innerHTML ='lorem'
+        console.log('baka'), title.innerHTML ='Chewbacca', msg.innerHTML ='Chewbacca is a Wookie born on Kashyyyk 200 years before the Battle of Yavin. Chewbacca fought in the Clone Wars on the Republic\'s side, but when The Galactic Empire was formed he was turned into a slave. Years later, Han saved him from slavery and since Chewbacca owed him a life debt, he decided to accompany Han on his smuggling adventures.'
     },
 
     onDragEnd : function(){
@@ -121,7 +121,7 @@ Draggable.create(boba,{
     type:'x,y',
     onDrag : function(){
         if(this.hitTest(point, '10'))
-        console.log('boba'), title.innerHTML ='boba', msg.innerHTML ='lorem'
+        console.log('Boba Fett'), title.innerHTML ='boba', msg.innerHTML ='Boba Fett is a human with Mandalorian armor and a bounty hunter. He is a clone of the famed Jango Fett and became a bounty hunter after the death of his father at the hands of Mace Windu. He would try and take his revenge on Windu but would ultimately fail. During the Empire\'s rule, he would often work very closely with Darth Vader and Jabba the Hutt and would even bring the famed Han Solo in carbonite to Jabba.'
     },
 
     onDragEnd : function(){
@@ -135,7 +135,7 @@ Draggable.create(leila,{
     type:'x,y',
     onDrag : function(){
         if(this.hitTest(point, '10'))
-        console.log('leila'), title.innerHTML ='leila', msg.innerHTML ='lorem'
+        console.log('leila'), title.innerHTML ='Leia Organa', msg.innerHTML ='Leia Organa was a Force-sensitive human female who served as the princess of Alderaan, a member of the Imperial Senate, a leader in the Alliance to Restore the Republic and New Republic, and the founding general of the Resistance, she was instrumental alongside her brother Luke Skywalker and comrade and later husband Han Solo in defeating the Empire’s forces on Endor. In the legends timeline, she would become a Jedi knight and serve the New Republic against the forces of Yuuzhan Vong and her very own son Jacen.'
     },
 
     onDragEnd : function(){
